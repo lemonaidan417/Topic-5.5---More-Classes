fcd9148e9d51f7a6336a47d700cb85d2056e41a4
Aidan Sutherland <339501421@GoTVDSB.ca>
2024-03-08 12:28:46 -0500

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5___More_Classes
+{
+    public class Die // Change 'internal' to 'public'
+    {
+        private int _roll;
+        private Random _generator;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+        public int Roll
+        {
+            get { return _roll; }
+            // set { _roll = value; }
+        }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+        public void DrawRoll()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----\r\n|   |\r\n| o |\r\n|   |\r\n-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----\r\n|o  |\r\n|   |\r\n|  o|\r\n-----");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----\r\n|o  |\r\n| o |\r\n|  o|\r\n-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----\r\n|o o|\r\n|   |\r\n|o o|\r\n-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----\r\n|o o|\r\n| o |\r\n|o o|\r\n-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----\r\n|o o|\r\n|o o|\r\n|o o|\r\n-----");
+            }
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_5._5___More_Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            Console.WriteLine(die1.Roll);
+            die1.DrawRoll();
+            die1.RollDie();
+            Console.WriteLine(die2.Roll);
+            die2.DrawRoll();
+            die2.RollDie();
+        }
+    }
+}
