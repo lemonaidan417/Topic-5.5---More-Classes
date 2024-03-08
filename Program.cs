adf8371e3743c1416e23b21fe3d81e648ea98c27
Aidan Sutherland <339501421@GoTVDSB.ca>
2024-03-08 13:08:56 -0500

    Need to code bets

--- Program.cs
+++ Program.cs
@@ -6,12 +6,29 @@
         {
             Die die1 = new Die();
             Die die2 = new Die();
-            Console.WriteLine(die1.Roll);
+            /* Console.WriteLine(die1.Roll);
             die1.DrawRoll();
             die1.RollDie();
             Console.WriteLine(die2.Roll);
             die2.DrawRoll();
-            die2.RollDie();
+            die2.RollDie();*/
+
+
+            double userAcctTotal = 100.00;
+            Console.WriteLine("Welcome to my dice game. You will start out with $100.\n" + 
+                "\r\nTo play my game, you will roll two dice. To profit you must roll what you bet, for example: " + 
+                "\nYou bet doubles and roll doubles, you win your bet");
+            Console.WriteLine();
+            Console.WriteLine("Roll            | Profit\r");
+            Console.WriteLine("                |\r");
+            Console.WriteLine("Doubles       \t| Win double their bet (2x). Ex. Bet is $5, user wins $10\r");
+            Console.WriteLine("                |\r");
+            Console.WriteLine("Not double  \t| Win half their bet. Ex. Bet is $10, user wins $5\r");
+            Console.WriteLine("                |\r");
+            Console.WriteLine("Even Sum   \t| Win their bet. Ex. Bet is $10, user wins $10\r");
+            Console.WriteLine("                |\r");
+            Console.WriteLine("Odd Sum     \t| Win their bet. Ex. Bet is $10, user wins $10\r");
+
         }
     }
 }
